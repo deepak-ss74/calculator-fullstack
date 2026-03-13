b15fbbda5410ca7ef8366bf02c586b6eed165113
deepakss-74 <kumardeepak805133@gmail.com>
2026-03-13 06:00:04 +0530

    updating logo

--- cal_frontend/components/AdminNavi.jsx
+++ cal_frontend/components/AdminNavi.jsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function  AdminNavi () {
   return (
     <header>
       <div className="bg-blue-500 text-white p-5 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">My LOGO</h1>
+        <Link href="/"> 
+        <Image
+          src="/next.svg"
+          width={150}
+          height={500}
+          alt="picture of the next"
+          alt="lawvriksh.com"          
+        /></Link>
 
         <ul className="flex gap-6">
           <li>
--- cal_frontend/components/Navigation.jsx
+++ cal_frontend/components/Navigation.jsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function  Navigation () {
   return (
     <header>
       <div className="bg-red-500 text-white p-5 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">My LOGO</h1>
+      <Link href="/"> 
+        <Image
+          src="/next.svg"
+          width={150}
+          height={500}
+          alt="picture of the next"
+          alt="lawvriksh.com"          
+        /></Link>
 
         <ul className="flex gap-6">
           <li>
